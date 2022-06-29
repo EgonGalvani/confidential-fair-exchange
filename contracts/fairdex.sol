ec4af6606eabffac998ba7bfdac1a7ba8ffc65a9
Egon <egongalvani@libero.it>
2022-06-29 20:00:21 +0200

    First compilation, placeholder values and solidity compiler version

--- contracts/fairdex.sol
+++ contracts/fairdex.sol
@@ -1,14 +1,17 @@
-pragma solidity >=0.6 <0.7.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+pragma experimental ABIEncoderV2;
 
-contract FairDEx {
+contract ConfidentialFairExchange {
+    
     address payable seller;
-    address payable buyer = BUYER_ADDRESS_HERE;
+    address payable buyer = payable(address(0)); // BUYER_ADDRESS_HERE;
 
     // Added 10 minutes to timeout in each function call.
     uint timeout;
 
-    uint constant depth= DEPTH_OF_MERKLE_TREE;
-    uint constant price = GOODS_PRICE;
+    uint constant depth= 16; // DEPTH_OF_MERKLE_TREE;
+    uint constant price = 1 wei; // GOODS_PRICE;
 
     bytes32 description;
     bytes32 public masterKey;
@@ -22,7 +25,7 @@ contract FairDEx {
     event ExecutionCompleted();
 
     constructor() public payable {
-        seller = msg.sender;
+        seller = payable(msg.sender);
     }
 
     // Deposit the goods' price and assign description.
@@ -34,13 +37,13 @@ contract FairDEx {
         emit PaidWithDescription();
         state = State.Paid;
         description = _description;
-        timeout = now + 10 minutes;
+        timeout = block.timestamp + 10 minutes;
     }
 
     // Refund to buyer if seller does not publish master key in time.
     function RefundToBuyer()
         public
-        only(buyer, State.Paid, now > timeout)
+        only(buyer, State.Paid, block.timestamp > timeout)
         payable
     {
         emit ExecutionCompleted();
@@ -56,7 +59,7 @@ contract FairDEx {
         emit PublishedMasterKey();
         state = State.Published;
         masterKey = _masterKey;
-        timeout = now + 10 minutes;
+        timeout = block.timestamp + 10 minutes;
     }
 
     // Complain about the goods by proving that 
@@ -64,7 +67,7 @@ contract FairDEx {
     // ii. and this committed subkey is part of the description.
     function RaiseObjection(uint _committed_ri, bytes32 _committedSubKey, bytes32[] memory _merkleTreePath)
         public
-        only(buyer, State.Published, now < timeout)
+        only(buyer, State.Published, block.timestamp < timeout)
         payable
     {
         bytes32 computedSubkey = keccak256(abi.encode(masterKey, _committed_ri));
@@ -91,7 +94,7 @@ contract FairDEx {
     // If objection timeouts, transfer the balance to seller and destruct the contract.
     function TransferToSeller()
         public
-        only(seller, State.Published, now > timeout)
+        only(seller, State.Published, block.timestamp > timeout)
     {
         emit ExecutionCompleted();
         state = State.Inactive;
