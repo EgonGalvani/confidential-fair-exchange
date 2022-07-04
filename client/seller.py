4d6db68be45d0641864475de01044d016219315b
Egon <egongalvani@libero.it>
2022-07-04 23:26:11 +0200

    Added withdraw function to seller

--- client/seller.py
+++ client/seller.py
@@ -5,11 +5,6 @@ import secrets
 from web3 import Web3 
 from web3.middleware import geth_poa_middleware
 
-# set up provider
-provider_rpc_url = "https://rpc-mumbai.maticvigil.com/"
-web3 = Web3(Web3.HTTPProvider("https://rpc-mumbai.maticvigil.com/"))
-web3.middleware_onion.inject(geth_poa_middleware, layer=0)
-
 def load_data(): 
   f = open("./data/settings.json", "r")
   settings = json.loads(f.read())
@@ -22,16 +17,22 @@ def load_data():
   f = open("./data/master_keys.txt", "r")
   master_keys = f.readlines()
   f.close() 
+  return settings, shared, master_keys
+settings, shared, master_keys = load_data()
 
+# set up provider
+web3 = Web3(Web3.HTTPProvider(settings["rpc_url"]))
+web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+def load_contract(): 
   contract_interface_f = open("./../build/ConfidentialFairExchange.json", "r")
   contract_interface = json.loads(contract_interface_f.read())
   contract_interface_f.close() 
 
   contract_address = web3.toChecksumAddress(settings["contract_address"])
   contract = web3.eth.contract(address=contract_address, abi=contract_interface["abi"])
-
-  return settings, shared, master_keys, contract 
-settings, shared, master_keys, contract = load_data() 
+  return contract
+contract = load_contract()
 
 # function publishFile(bytes32 _fileHash, uint _depth, uint _price, bytes32 _sellerPublicKey) public {
 def publish_file(file_hash, desc_depth, seller_public_key) :
@@ -78,6 +79,20 @@ def publish_master_key(encrypted_master_key, file_hash, purchase_ID):
   print("Publishing master key...")
   return sign_and_wait(transaction)
 
+# function withdraw(bytes32 _fileHash, bytes32 _purchaseID) 
+def withdraw(file_hash, purchase_ID):
+  nonce = web3.eth.getTransactionCount(settings["seller"]["address"])
+
+  transaction = contract.functions.withdraw(file_hash, purchase_ID).buildTransaction({
+      # 'gas': 3000000,
+      # 'gasPrice': web3.toWei(gas_price, 'gwei'),
+      'from': settings["seller"]["address"],
+      'nonce': nonce
+  })
+
+  print("Requesting withdraw...")
+  return sign_and_wait(transaction)
+
 def sign_and_wait(transaction):
   signed_txn = web3.eth.account.signTransaction(transaction, private_key=settings["seller"]["private_key"])
   tx_hash = web3.eth.sendRawTransaction(signed_txn.rawTransaction)
@@ -86,11 +101,13 @@ def sign_and_wait(transaction):
   print("Transaction correctly executed")
   return tx_receipt
 
+# first: init all files 
 for f in shared: # len(shared) 
-  # publish_file(f["file_hash"], f["desc_depth"], "") # TODO: add encryption key
+  publish_file(f["file_hash"], f["desc_depth"], "") # TODO: add encryption key
   publish_description(f["file_hash"], f["desc"])
 
   input("Press enter to continue...")
   publish_master_key()
 
+  # wait: withdraw
   break
